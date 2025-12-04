97fc7fdc0c15325aff11265a813e9eeba52b920a
Kleian <glenardmayor11@gmail.com>
2025-12-04 15:45:12 +0800

    asas

---
+++ database/migrations/2025_12_04_071753_drop_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('products');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->string('image')->nullable();
+            $table->string('name');
+            $table->text('detail')->nullable();
+            $table->float('price');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+        });
+    }
+};
