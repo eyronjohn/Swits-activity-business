cdfd9f4eebd05ba3369eb47d672d3e0d1a5ada67
eyronjohn <bitaraaron@gmail.com>
2025-12-04 16:00:51 +0800

    mino

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -14,28 +14,40 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::create([
-            'roles_id' => 1,
-            'name' => "Ian Kenneth Mendoza",
-            'email' => "admin01@sabonexpress.ph",
-            'password' => Hash::make('password')
-        ]);
+        // Create first user if doesn't exist
+        $user1 = User::firstOrCreate(
+            ['email' => 'admin01@sabonexpress.ph'],
+            [
+                'roles_id' => 1,
+                'name' => 'Ian Kenneth Mendoza',
+                'password' => Hash::make('password')
+            ]
+        );
 
-        $user->userDetail()->create([
-            'firstname' => "Ian Kenneth",
-            'lastname' => "Mendoza"
-        ]);
+        // Create user detail only if it doesn't exist
+        if (!$user1->userDetail) {
+            $user1->userDetail()->create([
+                'firstname' => 'Ian Kenneth',
+                'lastname' => 'Mendoza'
+            ]);
+        }
 
-        $user = User::create([
-            'roles_id' => 1,
-            'name' => "JC Basilio",
-            'email' => "admin02@sabonexpress.ph",
-            'password' => Hash::make('password')
-        ]);
+        // Create second user if doesn't exist
+        $user2 = User::firstOrCreate(
+            ['email' => 'admin02@sabonexpress.ph'],
+            [
+                'roles_id' => 1,
+                'name' => 'JC Basilio',
+                'password' => Hash::make('password')
+            ]
+        );
 
-        $user->userDetail()->create([
-            'firstname' => "JC",
-            'lastname' => "Basilio"
-        ]);
+        // Create user detail only if it doesn't exist
+        if (!$user2->userDetail) {
+            $user2->userDetail()->create([
+                'firstname' => 'JC',
+                'lastname' => 'Basilio'
+            ]);
+        }
     }
 }
